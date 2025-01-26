d9b94bbdf0dba4fd4647f905307efbf6063b6cca
karanp07x <karanp07x@gmail.com>
2025-01-26 15:49:18 +0530

    Added whatsapp

--- FRONTEND/src/components/resturantCardInfo.js
+++ FRONTEND/src/components/resturantCardInfo.js
@@ -1,35 +1,30 @@
 import React from "react";
-import { GlobalGlobalContext } from "../components/context";
 
-const RestaurantCard = ({ name, owner, description, phone, rating, email }) => {
-  const { isModelOpen, setIsModelOpen } = GlobalGlobalContext();
-
+const RestaurantCard = ({ name, owner, description, phone, rating, email, onPhoneClick, onEmailClick }) => {
   return (
-    <div className="w-[500px] mx-auto p-8 bg-white shadow-lg rounded-2xl border border-gray-200">
-      <h2 className="text-3xl font-bold text-gray-800 mb-4">{name}</h2>
-      <p className="text-gray-700 text-lg mb-3">
-        <span className="font-medium text-gray-900">Owner:</span> {owner}
+    <div className="p-4 bg-white rounded-xl shadow-lg">
+      <h3 className="text-xl font-bold text-gray-800">{name}</h3>
+      <p className="text-sm text-gray-600">Owned by {owner}</p>
+      <p className="text-gray-700 my-2">{description}</p>
+      <p className="text-gray-700">
+        Rating: <span className="font-bold">{rating} ⭐</span>
       </p>
-      <p className="text-gray-600 text-lg mb-5">{description}</p>
-      <p className="text-gray-700 text-lg mb-3">
-        <span className="font-medium text-gray-900">Phone:</span> {phone}
-      </p>
-      <p className="text-gray-700 text-lg mb-5">
-        <span className="font-medium text-gray-900">Avg Rating:</span> ⭐{" "}
-        {rating}/5
-      </p>
-      <p className="text-gray-700 text-lg mb-5">
-        <span className="font-medium text-gray-900">Email:</span> {email}
-      </p>
-      <button
-        className="px-6 py-3 bg-green-500 text-white text-lg rounded-lg shadow hover:bg-blue-600"
-        onClick={() => setIsModelOpen(true)}
-      >
-        Book now
-      </button>
-      <button className="px-6 py-3 bg-pink-500 text-white text-lg rounded-lg shadow hover:bg-blue-600 ml-4">
-        Add to fav
-      </button>
+      <div className="mt-4">
+        <button
+          onClick={() => onPhoneClick(phone)}
+          className="text-blue-600 hover:underline"
+        >
+          {phone}
+        </button>
+      </div>
+      <div className="mt-2">
+        <button
+          onClick={() => onEmailClick(email)}
+          className="text-blue-600 hover:underline"
+        >
+          {email}
+        </button>
+      </div>
     </div>
   );
 };
