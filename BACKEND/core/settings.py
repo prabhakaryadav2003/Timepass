644064b4a67f3a2b1ceb8105cca3bc09478fd5b6
Prabhakar <114614952+prabhakaryadav2003@users.noreply.github.com>
2025-08-10 00:15:19 +0530

    added whitenoise

--- BACKEND/core/settings.py
+++ BACKEND/core/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS += EXTERNAL_APPS
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Use compressed manifest storage for cache busting
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
